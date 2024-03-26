0224588a6275a9479cce0bf448ca27fda27ffadd
__KillerCodes <Killercodes@users.noreply.github.com>
2024-03-27 02:34:28 +0530

    Create investopedia.com.js

---
+++ scripts/investopedia.com.js
@@ -0,0 +1,11 @@
+//inJS: investopedia.com.js
+
+console.log("inJS loaded in investopedia.com");
+
+function hide(_field){
+	document.querySelector(_field).style.display='none';
+}
+
+hide(".article-meta");
+hide(".article-left-rail");
+hide(".related-recirc-section");
